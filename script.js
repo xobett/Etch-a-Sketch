acc903943fd944c18b4b76bfa5260d85b401e7c6
Cesar Betancourt <cessarr862@gmail.com>
2025-09-15 20:56:19 -0600

    Fix grid layout and resizing

--- script.js
+++ script.js
@@ -1,5 +1,55 @@
-// #region GRID SETTINGS
-let gridSize = 16;
-// #endregion GRID SETTINGS
+// GRID SETTINGS
+const grid = document.querySelector(".grid");
 
-const baseContainer = document.querySelector(".grid-container");
+const gridWidth = grid.clientWidth;
+const gridHeight = grid.clientHeight;
+
+// BUTTONS
+const solidModeBtn = document.querySelector(".solid-mode-button");
+const transparentModeBtn = document.querySelector(".transparent-mode-button");
+
+const blackColorBtn = document.querySelector(".black-color-button");
+const rgbColorBtn = document.querySelector(".rgb-color-button");
+
+solidModeBtn.addEventListener("click", () => resetGrid())
+
+
+drawGrid(16)
+
+function drawGrid(size) {
+    unitWidth = gridWidth / size;
+    unitHeight = gridHeight / size;
+    
+    for (let i = 0; i < size * size; i++){
+        const unit = document.createElement("div");
+        unit.setAttribute("class", "grid-unit");
+
+
+        unit.style.setProperty("--unit-width", `${unitWidth}px`);
+        unit.style.setProperty("--unit-height", `${unitHeight}px`);
+    
+        grid.appendChild(unit);
+    }
+}
+
+function resetGrid() {
+    if (!grid.firstChild) return;
+
+    while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+   }
+}
+
+let windowResized = false; 
+
+function resizeAlert() {
+    if (windowResized) return;
+    windowResized = true;
+
+    alert("Page will refresh to update grid size.");
+    location.reload();
+}
+
+window.addEventListener("resize", () => resizeAlert());
+
+
