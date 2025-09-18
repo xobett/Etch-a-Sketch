eb19ce03b037dcdb71dc546cb1f07fd0df0081f8
Cesar Betancourt <cessarr862@gmail.com>
2025-09-17 22:16:43 -0600

    Add rgb feature

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 //#region GRID SETTINGS
 const grid = document.querySelector(".grid");
 
-let gridWidth = grid.clientWidth;
-let gridHeight = grid.clientHeight;
-
 //#endregion GRID SETTINGS
 
 //#region BUTTONS
@@ -14,32 +11,61 @@ const blackColorBtn = document.querySelector(".black-color-button");
 const rgbColorBtn = document.querySelector(".rgb-color-button");
 
 const resizeBtn = document.querySelector(".resize-button");
-resizeBtn.addEventListener("click", () => resizeGrid());
 
 //#endregion BUTTONS
 
-let windowResized = false; 
-window.addEventListener("resize", () => resizeAlert());
+let transparencyMode = true;
+let rgbMode = false;
+
+const blackColor = "rgba(0, 0, 0, 1)";
+
+let targetGridSize = 10;
+let targetColor = blackColor;
+
+blackColorBtn.addEventListener("click", () => modifyRgbState(false));
+rgbColorBtn.addEventListener("click", () => modifyRgbState(true));
+resizeBtn.addEventListener("click", () => resizeGrid());
+
+window.addEventListener("resize", () => resetGrid());
 
 resetGrid();
-drawGrid(16);
 
-// FUNCTIONS
 
-function incrementAlpha(elem) {
+function interactWithGrid(elem) {
     const computedElem = window.getComputedStyle(elem);
-    console.log(computedElem.getPropertyValue("color"));
+    const color = computedElem.getPropertyValue("background-color");
+
+    targetColor = rgbMode ? getRandomColor() : blackColor;
+    let rgbValues;
+
+    if (transparencyMode) {
+        
+        if (color.startsWith("rgb(")) {
+            rgbValues = color.match(/\d+/g).map(Number);
+            rgbValues.push(1);
+        }
+        else if (color.startsWith("rgba(")) {
+            rgbValues = color.match(/[\d.]+/g).map(Number);
+        }
+    }
+
+    console.log(rgbValues);
+
+    if (color === "rgb(255, 255, 255)") {
+        elem.style.backgroundColor = targetColor;
+    }
 }
 
+//#region FUNCTIONS
+
 function resizeGrid() {
-    const querySize = prompt("Enter your desired grid size from 1 - 100.");
+    targetGridSize = prompt("Enter your desired grid size from 1 - 100.");
     resetGrid();
-    drawGrid(querySize);
 }
 
 function drawGrid(size) {
-    gridWidth = grid.clientWidth;
-    gridHeight = grid.clientHeight;
+    let gridWidth = grid.clientWidth;
+    let gridHeight = grid.clientHeight;
 
     unitWidth = gridWidth / size;
     unitHeight = gridHeight / size;
@@ -51,24 +77,43 @@ function drawGrid(size) {
         unit.style.setProperty("--unit-width", `${unitWidth}px`);
         unit.style.setProperty("--unit-height", `${unitHeight}px`);
 
-        unit.addEventListener("mouseenter", (e) => incrementAlpha(e.target))
+        unit.addEventListener("mouseenter", (e) => interactWithGrid(e.target))
     
         grid.appendChild(unit);
     }
 }
 
 function resetGrid() {
-    if (!grid.firstChild) return;
+    if (grid.firstChild) {
+            while (grid.firstChild) {
+                grid.removeChild(grid.firstChild);
+           }
+    }
 
-    while (grid.firstChild) {
-    grid.removeChild(grid.firstChild);
-   }
+   drawGrid(targetGridSize);
 }
 
-function resizeAlert() {
+function selectColor(colorValue){
+    targetColor = colorValue;
+}
+
+function modifyRgbState(value) {
+    rgbMode = value;
+
     resetGrid();
-    drawGrid(16)
 }
 
+function toggleTransparencyMode() {
+    transparencyMode = !transparencyMode;
+}
 
+function getRandomColor() {
+    let r, g, b;
 
+    r = Math.floor(Math.random() * 256);
+    g = Math.floor(Math.random() * 256);
+    b = Math.floor(Math.random() * 256);
+    
+    return `rgba(${r}, ${g}, ${b}, 1)`;
+}
+//#endregion FUNCTIONS
